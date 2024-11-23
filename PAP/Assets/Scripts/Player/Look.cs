f976d195fe1dbfdc0c1116f4a6bc249fa1139df5
Sena <senalikescake@gmail.com>
2024-11-23 11:16:59 +0000

    Added basic character movement (Looking around and walking)

--- PAP/Assets/Scripts/Player/Look.cs
+++ PAP/Assets/Scripts/Player/Look.cs
@@ -2,19 +2,27 @@ using UnityEngine;
 
 public class Look : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
     public Transform playerBody;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float mouseSensitivity = 300f;
+    float xRotation = 0f;
+    
     void Start()
     {
-        
+        //Lock cursos to center of the screen and hide it
+        Cursor.lockState =CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Reads the mouse input
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //Keeping track of rotation
+        xRotation -= mouseY;
+        //Not letting X rotation go below -90 or above 90, so that player doesn't look backwards by looking up or down too much
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //Aplying rotation
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- PAP/Assets/Scripts/Player/Movement.cs
+++ PAP/Assets/Scripts/Player/Movement.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+    public float speed = 12f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Get input for walking
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        //Create Movement Vector
+        Vector3 move = transform.right * x + transform.forward * z;
+        //Apply Movement
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
